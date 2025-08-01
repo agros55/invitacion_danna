67aae8a076ac8851f39969c1024b4d720e0669f5
agros55 <fernandoviveros95@gmail.com>
2025-07-31 23:43:07 -0300

    Primera edición

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function countdown() {
+    const targetDate = new Date("2025-10-18 21:00:00").getTime();
+    const ahora = new Date().getTime();
+    const timeLeft = targetDate - ahora;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}
+
+setInterval(countdown, 1000);
